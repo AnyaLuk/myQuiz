21189795004ffa488fbace9967ae8809d30bd346
AnyaLuk <76762756+AnyaLuk@users.noreply.github.com>
2021-07-26 11:09:50 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canva;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("yellow");
+
+  
+}
+
+play(){
+if(allContestants !== undefined){
+  fill("Blue");
+  textSize(20);
+  text("NOTE: Contestant who answered correct are highlighted in green!", 130,230)
+}
+}
